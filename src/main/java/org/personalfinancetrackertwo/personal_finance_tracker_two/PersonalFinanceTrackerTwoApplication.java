d33507f097f13271a22510baad143d787105fc4e
Gage Eisold <geisold@wgu.edu>
2025-06-21 20:35:29 -0500

    Initial commit.

---
+++ src/main/java/org/personalfinancetrackertwo/personal_finance_tracker_two/PersonalFinanceTrackerTwoApplication.java
@@ -0,0 +1,13 @@
+package org.personalfinancetrackertwo.personal_finance_tracker_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceTrackerTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceTrackerTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/personalfinancetrackertwo/personal_finance_tracker_two/PersonalFinanceTrackerTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personalfinancetrackertwo.personal_finance_tracker_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceTrackerTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
