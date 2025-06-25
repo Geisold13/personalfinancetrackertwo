b7ddd554baaea1f6eaf15251e1d0b3eecd337b3a
Gage Eisold <geisold@wgu.edu>
2025-06-25 12:50:26 -0500

    Random changes.

--- src/main/java/org/personalfinancetrackertwo/personal_finance_tracker_two/Entity/Role.java
+++ src/main/java/org/personalfinancetrackertwo/personal_finance_tracker_two/Entity/Role.java
@@ -1,4 +1,7 @@
 package org.personalfinancetrackertwo.personal_finance_tracker_two.Entity;
 
-public enum ROLE {
+public enum Role {
+
+    USER,
+    ADMIN
 }
--- src/main/java/org/personalfinancetrackertwo/personal_finance_tracker_two/Entity/User.java
+++ src/main/java/org/personalfinancetrackertwo/personal_finance_tracker_two/Entity/User.java
@@ -1,4 +1,95 @@
 package org.personalfinancetrackertwo.personal_finance_tracker_two.Entity;
 
-public class User {
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * User is a Spring Boot Entity that represents a real life user that uses
+ * the Personal Finance Tracker Two application.  This entity is directly mapped
+ * to a "users" table in the MySQL database.
+ */
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(name = "user_first_name", nullable = false)
+    private String userFirstName;
+
+    @Column(name = "user_last_name", nullable = false)
+    private String userLastName;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+
+    @Column(name = "user_role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role userRole;
+
+    @Column(name = "user_creation_date", nullable = false)
+    private Timestamp userCreationDate;
+
+    @Column(name = "user_last_sign_in", nullable = false)
+    private Timestamp userLastSignIn;
+
+    @Column(name = "user_last_sign_out", nullable = false)
+    private Timestamp userLastSignOut;
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
