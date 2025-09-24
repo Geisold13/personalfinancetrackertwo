42569f1f5b2e8f98a5cf6b9bbe47966128a22954
Gage Eisold <geisold@wgu.edu>
2025-09-24 13:08:31 -0500

    Continued work on root component.

--- src/main/resources/UI/personal-finance-tracker-two-frontend/src/components/app/app.component.ts
+++ src/main/resources/UI/personal-finance-tracker-two-frontend/src/components/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {AuthComponent} from "../auth/auth.component";
+import {NgIf} from "@angular/common";
+import {HttpClient, HttpClientModule, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {DashboardComponent} from "../dashboard/dashboard.component";
+
+import {AuthService} from "../../services/auth-service/auth.service";
+import {JwtService} from "../../services/jwt-service/jwt.service";
+import {UserService} from "../../services/user-service/user.service";
+import {TransactionsComponent} from "../transactions/transactions.component";
+import {TransactionstateService} from "../../services/transactionstate-service/transactionstate.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthComponent],
+  imports: [RouterOutlet, AuthComponent, NgIf, HttpClientModule, DashboardComponent, TransactionsComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'personal-finance-tracker-two-frontend';
+  user: string = "";
+
+
+
+  constructor(protected jwtService: JwtService, private userService: UserService, private transactionstateService: TransactionstateService, private router: Router) {
+
+  }
+
+  ngOnInit() {
+
+    this.userService.currentUser$.subscribe(user =>{
+      this.user = user;
+    })
+
+    if (!this.jwtService.isTokenValid()) {
+      this.transactionstateService.clearTransactions();
+      this.router.navigate(['/auth']);
+    }
+  }
+
+  signout() {
+    this.jwtService.removeToken();
+    this.transactionstateService.clearTransactions();
+    this.router.navigate(['/auth']);
+  }
+
+
+
+
 }
--- src/main/resources/UI/personal-finance-tracker-two-frontend/src/components/app/app.routes.ts
+++ src/main/resources/UI/personal-finance-tracker-two-frontend/src/components/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "../dashboard/dashboard.component";
+import {TransactionsComponent} from "../transactions/transactions.component";
+import {BudgetsComponent} from "../budgets/budgets.component";
+import {ReportsComponent} from "../reports/reports.component";
+import {GoalsComponent} from "../goals/goals.component";
+import {HelpingComponent} from "../helping/helping.component";
+import {AboutComponent} from "../about/about.component";
+import {SettingsComponent} from "../settings/settings.component";
+import {AuthComponent} from "../auth/auth.component";
+import {authGuard} from "../../guard/auth/auth.guard";
+import {signinGuard} from "../../guard/signin/signin.guard";
+import {NotificationsComponent} from "../notifications/notifications.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'auth', component: AuthComponent, canActivate: [signinGuard]},
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'transactions', component: TransactionsComponent},
+      {path: 'budgets', component: BudgetsComponent},
+      {path: 'reports', component: ReportsComponent},
+      {path: 'goals', component: GoalsComponent},
+      {path: 'notifications', component: NotificationsComponent},
+      {path: 'helping', component: HelpingComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'settings', component: SettingsComponent},
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    ]
+  },
+  {path: "**", redirectTo: 'dashboard'}
+];
